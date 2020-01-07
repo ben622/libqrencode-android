71a36c756e3d1628c1a5edeeea8261342f1d34de
zc622 <zc622>
2020-01-07 15:10:54 +0800

    add doc.

--- library/src/main/java/com/ben/android/qrencode/library/QREncode.java
+++ library/src/main/java/com/ben/android/qrencode/library/QREncode.java
@@ -18,25 +18,25 @@ public class QREncode {
     }
 
     /**
-     * @deprecated 以bmp格式输出在部分设备上可能存在兼容性问题，推荐使用{@link QREncode#encode(String, int, int)}
      * @param qrsource  二维码数据
      * @param prescaler 期望的二维码图片大小(像素点)，最终的大小可能小于该值。
      * @param output    输出文件，以bmp结尾
-     * @return          大于等于0 successful
+     * @return 大于等于0 successful
+     * @deprecated 以bmp格式输出在部分设备上可能存在兼容性问题，推荐使用{@link QREncode#encode(String, int, int)}
      */
     public static int encode(String qrsource, int prescaler, String output) {
         return encode(qrsource, prescaler, output, 0, 0, 0);
     }
 
     /**
-     * @deprecated 以bmp格式输出在部分设备上可能存在兼容性问题，推荐使用{@link QREncode#encode(String, int, int)}
      * @param qrsource  二维码数据
      * @param prescaler 期望的二维码图片大小(像素点)，最终的大小可能小于该值。
      * @param output    输出文件，以bmp结尾
      * @param r         二维码颜色通道R
      * @param g         二维码颜色通道G
      * @param b         二维码颜色通道B
-     * @return          大于等于0 successful
+     * @return 大于等于0 successful
+     * @deprecated 以bmp格式输出在部分设备上可能存在兼容性问题，推荐使用{@link QREncode#encode(String, int, int)}
      */
     public static int encode(String qrsource, int prescaler, String output, @IntRange(from = 0, to = 255) int r, @IntRange(from = 0, to = 255) int g, @IntRange(from = 0, to = 255) int b) {
         if (TextUtils.isEmpty(output)) {
@@ -50,15 +50,36 @@ public class QREncode {
         return nativeQREncode(qrsource, prescaler, output, r, g, b);
     }
 
+    /**
+     * @param qrsource  二维码数据
+     * @param prescaler 期望的二维码图片大小，最终的大小可能小于该值。
+     * @return 如果编码成功返回一个非空bitmap对象
+     */
     public static Bitmap encode(String qrsource, int prescaler) {
-        return nativeQREncode(qrsource, prescaler, Color.WHITE,Color.BLACK);
+        return nativeQREncode(qrsource, prescaler, Color.WHITE, Color.BLACK);
     }
+
+    /**
+     * @param qrsource  二维码数据
+     * @param prescaler 期望的二维码图片大小，最终的大小可能小于该值。
+     * @param color     二维码颜色，可以包含透明颜色通道
+     * @return 如果编码成功返回一个非空bitmap对象
+     */
     public static Bitmap encode(String qrsource, int prescaler, int color) {
         return nativeQREncode(qrsource, prescaler, Color.WHITE, color);
     }
-    public static Bitmap encode(String qrsource, int prescaler,int backgroundColor, int color) {
-        return nativeQREncode(qrsource, prescaler, backgroundColor,color);
+
+    /**
+     * @param qrsource        二维码数据
+     * @param prescaler       期望的二维码图片大小，最终的大小可能小于该值。
+     * @param backgroundColor 二维码背景颜色，可以包含透明颜色通道
+     * @param color           二维码颜色，可以包含透明颜色通道
+     * @return 如果编码成功返回一个非空bitmap对象
+     */
+    public static Bitmap encode(String qrsource, int prescaler, int backgroundColor, int color) {
+        return nativeQREncode(qrsource, prescaler, backgroundColor, color);
     }
+
     /**
      * @param qrsource  二维码数据
      * @param prescaler 期望的二维码图片大小，最终的大小可能小于该值。
@@ -66,10 +87,17 @@ public class QREncode {
      * @param r         二维码颜色通道R
      * @param g         二维码颜色通道G
      * @param b         二维码颜色通道B
-     * @return          大于等于0 successful
+     * @return 大于等于0 successful
      */
     private native static int nativeQREncode(String qrsource, int prescaler, String output, @IntRange(from = 0, to = 255) int r, @IntRange(from = 0, to = 255) int g, @IntRange(from = 0, to = 255) int b);
 
-    private native static Bitmap nativeQREncode(String qrsource, int prescaler,  int backgroundColor,int color);
+    /**
+     * @param qrsource        二维码数据
+     * @param prescaler       期望的二维码图片大小，最终的大小可能小于该值。
+     * @param backgroundColor 二维码背景颜色，可以包含透明颜色通道
+     * @param color           二维码颜色，可以包含透明颜色通道
+     * @return 如果编码成功返回一个非空bitmap对象
+     */
+    private native static Bitmap nativeQREncode(String qrsource, int prescaler, int backgroundColor, int color);
 
 }
